abfb8a786b5d50d02dfb31dc5bdd1aeeea8dde02
Michael Houze <michael.houze@gmail.com>
2014-03-17 15:23:28 -0400

    Homework 6 - Home Page
    - Update the home page
    - Created the Pet model and controller

---
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public byte[] Picture { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
