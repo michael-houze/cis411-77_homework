6ce76fd5f7c0bd054ebbc45018c158d7df1c6b67
michael-houze <michael.houze@gmail.com>
2014-04-15 10:23:41 -0400

    Homework 6 - Final Changes
    - Made changes to the database connection based on troubleshooting done
    in class.

--- homework6/PetStore/PetStore/Models/Pet.cs
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -10,12 +10,12 @@ namespace PetStore.Models
     public class Pet
     {
         public int ID { get; set; }
-        public byte[] Picture { get; set; }
+        public string Picture { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime DateReceived { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } 
     }
 
     public class PetDBContext : DbContext
